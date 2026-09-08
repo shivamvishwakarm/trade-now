a906e538555850500ceee32c755459ab17fa8a78
shivamvishwakarm <vishwakarmashivam2003@gmail.com>
2026-09-08 10:35:56 +0530

    feat: configure air for live loading and group routes

--- internal/http/router.go
+++ internal/http/router.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/shivamvishwakarm/trade-now/internal/websocket"
@@ -19,14 +20,17 @@ func NewRouter(deps RouterDeps) *gin.Engine {
 
 	router.Use(gin.Recovery())
 
-	router.GET("/ping", func(ctx *gin.Context) {
+	routesV1 := router.Group("/api/v1")
+
+	routesV1.GET("/health", func(ctx *gin.Context) {
 
 		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Pong",
+			"message":   "healthy",
+			"timestamp": time.Now().UTC(),
 		})
 	})
 
-	router.GET("/connect", deps.WsHandler.Handle)
+	routesV1.GET("/connect", deps.WsHandler.Handle)
 
 	return router
 
